95d37bf5ea9e43ac49a6101818c2755cb5c4da6e
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2018-10-04 20:53:11 +0300

    Some improvements

--- discovering-scala-I/src/main/scala/lesson/MatchDay.scala
+++ discovering-scala-I/src/main/scala/lesson/MatchDay.scala
@@ -2,7 +2,7 @@ package lesson
 
 case class MatchDay(day: Int, matches: List[Match]) extends Comparable[MatchDay] {
 
-  val teams = (matches.map(_.home).toSet ++ matches.map(_.away).toSet).toList
+  val teams: List[Team] = (matches.map(_.home) ++ matches.map(_.away)).distinct
 
   def numberOfMatches: Int = matches.size
 
@@ -27,40 +27,60 @@ object MatchDay {
 
   /** Returns a set of matches with more than 3 goals total */
   def excitingMatches(matchDays: List[MatchDay]): List[Match] =
-  matchDays.flatMap(_.matches).filter(_match => _match.goalsHome + _match.goalsAway > 3)
+    matchDays.flatMap(_.matches.filter(m => m.goalsHome + m.goalsAway > 3))
+
+  def excitingMatches1(matchDays: List[MatchDay]): List[Match] =
+    matchDays.flatMap(_.matches).filter(_match => _match.goalsHome + _match.goalsAway > 3)
+
+  def excitingMatches2(matchDays: List[MatchDay]): List[Match] =
+    for {
+      day <- matchDays
+      m <- day.matches
+      if m.goalsHome + m.goalsAway > 3
+    } yield {
+      m
+    }
 
   /** Returns a set of all winning teams of the given match day */
-  def winners(matchDay: MatchDay): Set[Team] = {
-    matchDay.matches.filter(_match => _match.goalsHome - _match.goalsAway != 0).map { _match_ =>
-      if (_match_.goalsAway > _match_.goalsHome) _match_.away
-      else _match_.home
-    }.toSet
-  }
+  def winners(matchDay: MatchDay): Set[Team] =
+    matchDay.matches
+      .filter(m => m.goalsAway != m.goalsHome)
+      .map(m => if (m.goalsAway > m.goalsHome) m.away else m.home)
+      .toSet
 
   /**
     * Returns the number of red cards per section, assuming a football halftime has a fixed duration of 45 minutes
     */
-  def redCardsPerSection(matchdays: List[MatchDay]): (Int, Int) = matchdays.flatMap(_.matches).foldRight((0, 0)) {
-    (_match_, result) =>
-      _match_.redCard match {
-        case Some(card) if card.minute < 45 => (result._1 + 1, result._2)
-        case Some(card) if card.minute >= 45 => (result._1, result._2 + 1)
-        case None => result
+  def redCardsPerSection(matchdays: List[MatchDay]): (Int, Int) =
+    matchdays.flatMap(_.matches).foldRight((0, 0)) { (m, acc) =>
+      m.redCard match {
+        case Some(card) if card.minute < 45 => (acc._1 + 1, acc._2)
+        case Some(card) if card.minute >= 45 => (acc._1, acc._2 + 1)
+        case None => acc
       }
-  }
+    }
 
   /**
     * Return a set of those teams which play on all given match days
     */
   def playsOnAllMatchdays(matchdays: List[MatchDay]): Set[Team] = {
-    val teams = matchdays.flatMap(_.teams)
-    teams.filter(team => teams.count(_ == team) == matchdays.size).toSet
+    val allTeams = matchdays.flatMap(_.teams)
+    allTeams.filter(team => allTeams.count(_ == team) == matchdays.size).toSet
   }
 
   /**
     * Returns a map,
     */
-  def mappedByParticipant(matchdays: List[MatchDay]): Map[Team, Set[MatchDay]] = {
+  def mappedByParticipant(matchdays: List[MatchDay]): Map[Team, Set[MatchDay]] =
+    matchdays
+      .flatMap(_.teams)
+      .distinct
+      .map(team =>
+        team -> matchdays.filter(day => day.matches.map(_.away).contains(team) || day.matches.map(_.home).contains(team)).toSet
+      )
+      .toMap
+
+  def mappedByParticipant1(matchdays: List[MatchDay]): Map[Team, Set[MatchDay]] = {
     val allTeams = matchdays.flatMap(_.teams)
 
     allTeams.map { team =>
